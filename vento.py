2e03f245cbd2db70a754494ec096c76c0177383e
Mauro Costa Sollar <maurosollar@gmail.com>
2023-09-22 15:41:31 -0300

    Update vento.py

--- vento.py
+++ vento.py
@@ -1,10 +1,10 @@
 import esp32, network, json, ssd1306, utils, math
 from machine import Pin, SoftI2C, ADC, Timer
 from micropyserver import MicroPyServer
-from time import sleep
+from time import sleep, ticks_ms, ticks_diff
 
 WIND_DIR_PIN = 4
-WIND_SPD_PIN = 6
+WIND_SPD_PIN = 35
 
 wind_dir_pin = ADC(Pin(WIND_DIR_PIN))
 wind_speed_pin = Pin(WIND_SPD_PIN, Pin.IN)
@@ -16,9 +16,9 @@ i2c = SoftI2C(scl=Pin(32), sda=Pin(33), freq=400000) # IO33=485_EN / IO32=CFG
 display = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 # Visto no site do MicroPython - Wireless-Tag's WT32-ETH01 v1.4
-lan = network.LAN(mdc=machine.Pin(23), mdio=machine.Pin(18),
+lan = network.LAN(mdc=Pin(23), mdio=Pin(18),
                   phy_type=network.PHY_LAN8720, phy_addr=1,
-                  power=machine.Pin(16))
+                  power=Pin(16))
 
 if not lan.active():
     lan.active(True)
@@ -26,27 +26,26 @@ if not lan.active():
 while not lan.isconnected():
     pass
 
-global contador = 0
-global raio_anemometro = 147
-global direcao
-global velocidade
+wind_speed_last_int = ticks_ms()
+contador = 0
+raio_anemometro = 147
 
 sleep(3)
 endip = lan.ifconfig()[0]
 display.fill(0)
-display.text('End. IP:', 0, 0, 1)
-display.text(endip, 0, 16, 1)
+display.text('IP:', 0, 0, 1)
+display.text(endip, 32, 0, 1)
 display.show()
 
 
-def calcula()
+def calcula(timer):
     global contador
     global raio_anemometro
     global direcao
     global velocidade
     rpm = contador
     contador = 0
-    var_adc = wind_dir_pin.read_u16() # Faixa 0-65535
+    val_adc = wind_dir_pin.read_u16() # Faixa 0-65535
     if val_adc <= 0.27:
         dir_grau = 315
         dir_nome = "Noroeste"
@@ -71,21 +70,22 @@ def calcula()
     else:
         dir_grau = 0
         dir_nome = "Norte"
-    direcao = dir_nome
-    velocidade = round((((4 * math.pi * raio_anemometro * rpm)/60)/1000)*3.6,1)
-    speed = "RPM / Velocidade KM/h: " + str(rpm) + ' / ' + str(velocidade))
+    velocidade = round((((4 * math.pi * raio_anemometro * rpm)/5)/1000)*3.6,1)
+    print('calcula: ', val_adc, 'direção:', dir_nome, 'RPM:', rpm, 'Velocidade:', velocidade)
     winddir = "Direcao: " + str(dir_nome)
     display.fill(0)
-    display.text('End. IP:', 0, 0, 1)
-    display.text(endip, 0, 16, 1)
-    display.text(speed, 0, 32, 1)
-    display.text(winddir, 0, 48, 1)        
+    display.text('IP:', 0, 0, 1)
+    display.text(endip, 32, 0, 1)
+    display.text('KM/h:' + str(velocidade), 0, 16, 1)
+    display.text('RPM:' + str(rpm), 0, 32, 1)
+    display.text('Dir.:' + dir_nome, 0, 48, 1)   
     display.show()
 
 def wind_speed_int(irq):
-    global wind_speed_last_int  # anti repique por software do reed switch mecânico
+    global wind_speed_last_int
     global contador
-    if ticks_diff(ticks_ms(), wind_speed_last_int) > 5:  # Não menos que 5ms entre pulsos
+    print('vento', contador)
+    if ticks_diff(ticks_ms(), wind_speed_last_int) > 5:  # Não menos que 5ms entre pulsos, tratamento de debounce
         wind_speed_last_int = ticks_ms()
         contador += 1
 
@@ -99,9 +99,10 @@ def winddir_speed(request):
     server.send("Connection: close\n\n")      
     server.send(json_str)
 
-wind_speed_last_int = ticks_ms()
-wind_speed_pin.irq(trigger=Pin.IRQ_RISING, handler=wind_speed_int)
-data_check_timer.init(period=1000, mode=Timer.PERIODIC, callback=calcula)
+
+wind_speed_pin.irq(trigger=Pin.IRQ_RISING, handler=wind_speed_int) # executa rotina "wind_speed_int" toda vez que o pino de entrar entrar em nível alto
+
+data_check_timer.init(period=5000, mode=Timer.PERIODIC, callback=calcula) # executa rotina "calcula" de 5 em 5 segundos
 
 server = MicroPyServer()
 server.add_route("/", winddir_speed)
